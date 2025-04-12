5bc30b8881cd8ed83bb4394f09c16fc8b1f978ea
Mehanth <mehanth2310579@ssn.edu.in>
2025-04-13 00:15:15 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MemoryMatch from './MemoryMatch';
+//             let size = Math.random() * 10 + 5; // Size between 5 and 15
+function App() {
+
+  return (
+    <>
+      <MemoryMatch />
+    </>
+  )
+}
+
+export default App
